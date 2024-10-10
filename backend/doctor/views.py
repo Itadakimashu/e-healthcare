287f8061146c630050f0128556dcb4d35c908c06
Itadakimashu <fazlyfardinchowdhury@gmail.com>
2024-10-11 00:51:17 +0600

    implemented doctor registration and user login

--- backend/doctor/views.py
+++ backend/doctor/views.py
@@ -3,9 +3,12 @@ from django.http import JsonResponse
 
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import DoctorProfile
-from .serializers import DoctorProfileSerializer
+from .serializers import DoctorProfileSerializer,DoctorRegistrationSerializer
 from .constants import designations, specialists
 
 
@@ -17,6 +20,16 @@ class DoctorProfileViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = DoctorProfileSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['specialist__name','designation']
+
+
+class DoctorRegistrationView(APIView):
+    serializer_class = DoctorRegistrationSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = DoctorRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def get_designations_and_specialists(request):
--- backend/userauth/views.py
+++ backend/userauth/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
 
-from rest_framework import viewsets
+from rest_framework import viewsets,generics, permissions
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.response import Response
 
 from .serializers import CustomUserSerializer
 
@@ -14,3 +19,26 @@ class CustomUserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [IsAuthenticated]
+
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = TokenObtainPairSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token = AccessToken.for_user(user)
+            return Response({
+                'access': str(token),
+                'user':{
+                    'username': user.username,
+                    'email': user.email,
+                    'id': user.id,
+                    'usertype': user.usertype,
+                    'name': user.name
+                }
+            })
+        return Response({'error': 'Invalid credentials'}, status=400)

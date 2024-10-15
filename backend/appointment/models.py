dc448f97518a1a6d8e1cc3bd9a105b951fd88578
Itadakimashu <fazlyfardinchowdhury@gmail.com>
2024-10-16 03:17:23 +0600

    implemented appointment update and added fields symptoms,doctor_review

--- backend/appointment/models.py
+++ backend/appointment/models.py
@@ -12,10 +12,13 @@ from doctor.constants import DAYS_OF_WEEK_CHOICES
 class Appointment(models.Model):
     doctor = models.ForeignKey(DoctorProfile,on_delete=models.CASCADE,related_name='appointments')
     patient = models.ForeignKey(PatientProfile,on_delete=models.CASCADE,related_name='appointments')
+    symptoms = models.TextField(blank=True,null=True)
+    doctor_review = models.TextField(blank=True,null=True)
     status = models.CharField(max_length=20,choices=APPOINTMENT_STATUS,default='pending')
     serial_number = models.IntegerField()
     day = models.CharField(max_length=20,choices=DAYS_OF_WEEK_CHOICES)
     date = models.DateField()
+
 
     def __str__(self):
         return f"{self.doctor.user.name} - {self.patient.user.name} - {self.date}"
--- backend/e_healthcare/urls.py
+++ backend/e_healthcare/urls.py
@@ -16,7 +16,7 @@ from doctor.views import (
 )
 from patient.views import PatientProfileViewSet,PatientRegistrationView,get_blood_groups_and_genders
 
-from appointment.views import AppointmentViewSet,AppointmentCreateView
+from appointment.views import AppointmentViewSet,AppointmentCreateView,AppointmentUpdateView
 
 router = routers.DefaultRouter()
 router.register(r'users', CustomUserViewSet)
@@ -28,15 +28,20 @@ router.register(r'appointments', AppointmentViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    
     path('doctor-register/', DoctorRegistrationView.as_view()),
     path('patient-register/', PatientRegistrationView.as_view()),
+    
     path('login/', UserLoginView.as_view(),name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 
+    path('create-appointment/', AppointmentCreateView.as_view()),
+    path('update-appointment/<int:pk>/', AppointmentUpdateView.as_view()),
+    
     path('get-designations-and-specialists/', get_designations_and_specialists),
     path('get-blood-groups-and-genders/', get_blood_groups_and_genders),
-    path('create-appointment/', AppointmentCreateView.as_view()),
+    
 
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
